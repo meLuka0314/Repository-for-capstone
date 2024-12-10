93102164f65cad59d003b591e3bfcbdcf4aade5e
Luka <luka.sullivan@gmail.com>
2024-12-10 12:17:29 -0600

    started on project
    started on project

---
+++ Capstone Project/sketch.js
@@ -0,0 +1,27 @@
+// Captone project
+// Luka Sullivan
+// December 10, 2024
+//
+
+let gameStarted = false;
+
+function setup() {
+createCanvas(window.Width, window.Height);
+
+if (gameStarted === false) {
+    showMenu();
+}
+}
+
+function draw() {
+
+
+
+
+function showMenu() {
+    noStroke();
+    textAlign(CENTER, CENTER);
+    text("Puck Soccer", 10, 10);
+    textSize(20);
+}
+}
