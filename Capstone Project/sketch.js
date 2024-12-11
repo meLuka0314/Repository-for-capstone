7811cdadcd33ae4c9b2168eb237bdac50fc13af9
Luka <luka.sullivan@gmail.com>
2024-12-11 12:31:47 -0600

    Created the court and puck
    I created the court and the puck

--- Capstone Project/sketch.js
+++ Capstone Project/sketch.js
@@ -4,24 +4,104 @@
 //
 
 let gameStarted = false;
+let puck;
 
 function setup() {
-createCanvas(window.Width, window.Height);
-
-if (gameStarted === false) {
+  createCanvas(windowWidth, windowHeight);
+  puck = new Puck();
+  if (!gameStarted) {
     showMenu();
-}
+  }
 }
 
 function draw() {
+  if (gameStarted) {
+    drawField();
+    puck.display();
+    puck.move();
+  }
+}
 
-
-
-
+//creates the menu
 function showMenu() {
-    noStroke();
-    textAlign(CENTER, CENTER);
-    text("Puck Soccer", 10, 10);
-    textSize(20);
+  background(0);
+  fill(255);
+  textAlign(CENTER, CENTER);
+  textSize(100);
+  text("Puck Soccer", width/2, 250);
+  textSize(30);
+  text("Press Enter to start", width/2, height/2 + 20);
 }
+
+//when enter key is pressed gameStarted is true
+function keyPressed() {
+  if (keyCode === ENTER) {
+    gameStarted = true;
+  }
 }
+
+function drawField() {
+  background(0, 180, 0); //sets background color for the field
+
+  //makes the field boundaries
+  stroke(255);
+  strokeWeight(4);
+  noFill();
+  rect(300, 120, width - 600, height - 240);
+
+  //creates center line
+  line(width/2, 120, width/2, height - 120);
+
+  //creates cenetr circle
+  ellipse(width/2, height/2, 300);
+  
+  //creates goals 
+  rect(280, height/2 - 120, 20, 240);
+  rect(width - 300, height/2 - 120, 20, 240);
+
+  //creates center points
+  ellipse(width/2, height/2, 7, 7);
+}
+
+class Puck {
+  constructor() {
+    this.x = width/2;
+    this.y = height/2;
+    this.diameter = 60;
+    this.speed = 4;
+  }
+
+  display() {
+    fill(200, 0, 0);
+    strokeWeight(1);
+    ellipse(this.x, this.y, this.diameter);
+  }
+
+  move() {
+    if (keyIsDown(LEFT_ARROW)) {
+      this.x -= this.speed + 0.5;
+    }
+
+    if (keyIsDown(RIGHT_ARROW)) {
+      this.x += this.speed + 0.5;
+    }
+
+    if (keyIsDown(UP_ARROW)) {
+      this.y -= this.speed;
+    }
+
+    if (keyIsDown(DOWN_ARROW)) {
+      this.y += this.speed;
+    }
+  }
+
+
+
+
+
+
+
+}
+
+
+
